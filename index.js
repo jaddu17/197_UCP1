ae3687f7fa87486ee09068468b101b29fb63ad3b
jaddu17 <shafajaddu@gmail.com>
2025-11-04 13:58:43 +0700

    membuat file index.js

---
+++ index.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const app = express();
+const db = require('./models');
+const Kandang = require('./models/Kandang');
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended : false}));
+
+app.listen(PORT, async () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+db.sequelize.sync().then((result) => {
+    app.listen(3000, () => {
+        console.log('Server Started');
+    })
+})
+
+    .catch((err) => {
+        console.log(err);
+})
+
+app.post('/kandang', async (req, res) => {
+    const data = req.body;
+    try{
+        const kandang = await db.Kandang.create(data);
+        res.send(kandang);
+    } catch (error) {
+        res.status(500).send({message: error.message});
+    }
+});
+
+app.get('/kandang', async (req, res) => {
+    try{
+        const kandang = await db.Kandang.findAll();
+        res.send(kandang);
+    } catch(error){
+        res.status(500).send({message: error.message});
+    }
+});
+
+app.put('/kandang/:id', async (req, res) => {
+    const id = req.params.id;
+    const data = req.body;
+    try{
+        const kandang = await db.Kandang.findByPk(id);
+        if(!kandang){
+            return res.status(404).send({message: 'Kandang not found'});
+        }
+        await kandang.update(data);
+        res.send({message: "Kandang berhasil diupdate"}, kandang);
+    } catch(error) {
+        res.status(500).send({message: error.message});
+    }
+});
+
+app.delete('/kandang/:id', async(req, res) => {
+    const id = req.params.id;
+    try{
+        const kandang = await db.Kandang.findByPk(id);
+        if (!kandang){
+            return res.status(404).send({message: 'Kandang not found'});
+        }
+        await kandang.destroy();
+        res.send({message: "Kandang berhasil dihapus"});
+    }catch (error) {
+        res.status(500).send({message: error.message});
+    }
+});
